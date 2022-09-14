b7b1444a4dbe68fb786afa85c5d72fce034220e7
RugerClaus Falck <106636510+RugerClaus@users.noreply.github.com>
2022-09-14 00:26:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './CSS/App.css'
+import Header from './Components/Header/Header'
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
+}
---
+++ src/Components/Header/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li className='navItem'>Home</li>
+        <li className='navItem'>About Us</li>
+        <li className='navItem'>Our Services</li>
+        <li className='navItem'>Outreach</li>
+        <li className='navItem'>Clients & Partners</li>
+        <li className='navItem'>Contact Us</li>
+      </ul>
+    </nav>
+  )
+}
